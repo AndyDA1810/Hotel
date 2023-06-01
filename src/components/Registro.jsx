962b35c310d52a9a7be0aba8a3a39d5f588bb0aa
Andy Dominguez <125949338+AndyDA1810@users.noreply.github.com>
2023-06-01 08:57:30 -0500

    Update Registro.jsx

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -251,6 +251,7 @@ const Registro = (props) => {
                   <b>Comentarios:</b> {elemento.comentarios}<br></br>
                   <button className="btn btn-success float-end mx-2" onClick={()=>editar(elemento)} >Editar</button>
                   <button className="btn btn-danger float-end mx-2" onClick={()=>eliminarDato(elemento.id)}>Eliminar</button>
+<button className="btn btn-danger float-end mx-2" onClick={()=>eliminarDato(elemento.id)}>Eliminar</button>
                   </li>
                 )
               )
