817047f236dde133ec21edbe7f1d2a667fcbdadf
Andy Dominguez <125949338+AndyDA1810@users.noreply.github.com>
2023-06-01 11:49:42 -0500

    Update Inicio.jsx

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -9,7 +9,7 @@ const Inicio = () => {
     <section class="presentacion">
       <img src="./Imagendehotel.jpeg" alt="Imagen de presentación del hotel"></img>
       <div>
-        <h2>Bienvenido a el Hotel Turquoise Moon</h2>
+        <h2>Bienvenido a Turquoise Moon</h2>
         <p>Descubre una experiencia única de hospedaje en el corazón de la ciudad. Disfruta de nuestras amplias habitaciones, nuestra exquisita gastronomía y nuestras modernas instalaciones.</p>
         <a href="#">Ver más</a>
       </div>
